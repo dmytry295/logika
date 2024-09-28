1805bbf6d93641ba8c9ff1aeec411946803a5158
dmytry295 <d.dmytriv2010@gmail.com>
2024-09-28 19:28:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,106 @@
-with open("quotes.txt","r",encoding= "utf-8") as file:
-    data = file.read()
-    print(data)
+from PyQt5.QtCore import*
+from PyQt5.QtWidgets import*
+#--------------------
+# Вікно програми
+#--------------------
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("Smart Notes")
+window.resize(800,600)
+window.move(0,0)
+window.setStyleSheet("background-color : rgb(234,255,205); font-size: 15px; font-family: Font") # створюємо фон
 
-author = input("Хто написав ці рядки?")
+#--------------------
+# створюємо інтерфейс
+#--------------------
+text_editor = QTextEdit() # поле для тексту замітки
+text_editor.setPlaceholderText("Введіть текст замітки:")
 
-with open("quotes.txt","a",encoding= "utf-8") as file:
-    file.write("author + \n")
-answer = input("Бажаєте додати ще одну цитату? (так / ні) - ")
-answer = answer.lower()
+list_widget_1 = QListWidget()
+list_widget_2 = QListWidget()
 
-while True:
-    if answer == "так":
-        quotes = input("Введіть цитату:")
-        author = input("Введіть автора:")
+text_searcher = QLineEdit()
+text_searcher.setPlaceholderText("Введіть текст:") # пошук по тексту
 
-        file = open ("quotes.txt","a", encoding = "utf-8")
-        file.write(quotes + "\n")
-        file.write("( "+ author + ")"+ "\n")
-        file.close()
+tag_seacher = QLineEdit()
+tag_seacher.setPlaceholderText("Введіть тег:") #  пошук по тегу
+#--------------------
+# ставимо кнопки
+#--------------------
+make_note = QPushButton()
+make_note.setText("Створити замітку") # створюєм
 
-    else :
-        break
+delet_note = QPushButton()
+delet_note.setText("Видалити замітку") # видаляєм
+
+save_note = QPushButton()
+save_note.setText("Зберигти замітку") # Зберігаємо
+
+seacher_for_text = QPushButton()
+seacher_for_text.setText("Шукати за текстом")
+
+seacher_for_tag = QPushButton()
+seacher_for_tag.setText("Шукати за тегом")
+
+add_to_note = QPushButton()
+add_to_note.setText("Додати тег до замітки")
+
+unpin_to_note = QPushButton()
+unpin_to_note.setText("Відкрипити тег до замітки")
+
+convert_to_txt = QPushButton()
+convert_to_txt.setText("Конвертувати до txt формату")
+
+action_theme_btn = QPushButton()
+action_theme_btn.setText("Змінити тему")
+
+#--------------------
+# макет
+#--------------------
+row1 = QHBoxLayout()
+row1.addWidget(make_note)
+row1.addWidget(delet_note)
+
+row2 = QHBoxLayout()
+row2.addWidget(add_to_note)
+row2.addWidget(unpin_to_note)
 
 
-print("зчитуємо фінальний файл:")
-with open("quotes.txt","r",encoding= "utf-8") as file:
-    for line in file: 
-        print(line)
+col1 = QVBoxLayout()
+col1.addWidget(text_editor)
+
+col2 = QVBoxLayout()
+col2.addWidget(QLabel("Список заміток:"))
+col2.addWidget(list_widget_1)
+col2.addLayout(row1)
+col2.addWidget(save_note)
+col2.addWidget(QLabel("Список тегів:"))
+col2.addWidget(list_widget_2)
+col2.addWidget(QLabel("Пошук даних:"))
+col2.addWidget(text_searcher)
+col2.addWidget(tag_seacher)
+col2.addLayout(row2)
+
+col2.addWidget(seacher_for_tag)
+col2.addWidget(seacher_for_text)
+col2.addWidget(convert_to_txt)
+col2.addWidget(action_theme_btn)
+#--------------------
+# Злиття
+#--------------------
+layout_note =  QHBoxLayout()
+layout_note.addLayout(col1)
+layout_note.addLayout(col2)
+
+window.setLayout(layout_note)
 
 
 
 
 
+
+
+
+
+window.show()
+app.exec_()

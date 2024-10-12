1dfaaf2cb95e568c60aeddc6af394c97afc7ebc2
dmytry295 <d.dmytriv2010@gmail.com>
2024-10-12 19:25:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,80 +1,102 @@
-from PyQt5.QtCore import*
-from PyQt5.QtWidgets import*
+# - Підключаємо бібліотеки
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
 import json
 
 
-def writeToFile():   
-    with open("notes.json","w") as file:
-        json.dump(notes, file,sort_keys=True)
+# -------------------------- [Створюємо структуру заміток ідентичну json файлу] ------------------------------------------
+
+# - функція для запису в файл
+def writeToFile():
+    with open("notes.json", "w") as file:   # Записуємо нашу структуру - сортовано
+        json.dump(notes, file, sort_keys=True)
+# --------------------------------------------------------------------
 
 
-#--------------------
-# Вікно програми
-#--------------------
+'''Інтерфейс програми'''
+# ---------------------------
+#   Вікно програми
+# ---------------------------
 app = QApplication([])
 window = QWidget()
+main_width, main_height = 800, 600  # початкові розміри головного вікна
 window.setWindowTitle("Smart Notes")
-window.resize(800,600)
+window.resize(main_width,main_height)
 window.move(0,0)
-window.setStyleSheet("background-color : rgb(234,255,205); font-size: 15px; font-family: Font") # створюємо фон
+window.setStyleSheet('background-color:rgb(235, 231, 255);font-size:15px')
 
-#--------------------
-# створюємо інтерфейс
-#--------------------
-text_editor = QTextEdit() # поле для тексту замітки
-text_editor.setPlaceholderText("Введіть текст замітки:")
+# ---------------------------
+#     Eлементи інтерфейсу
+# ---------------------------
+text_editor = QTextEdit()                                           # Введення тексту замітки
+text_editor.setStyleSheet(' background-color:#CCFFFF;')
+text_editor.setPlaceholderText('Введіть текст...')
 
-list_widget_1 = QListWidget()
-list_widget_2 = QListWidget()
+list_widget_1 = QListWidget()                                       # Список заміток
+list_widget_1.setStyleSheet('background-color:#CCFFFF;')
 
-text_searcher = QLineEdit()
-text_searcher.setPlaceholderText("Введіть текст:") # пошук по тексту
+list_widget_2 = QListWidget()                                       # Список (тегів)
+list_widget_2.setStyleSheet('background-color:#CCFFFF;')
 
-tag_seacher = QLineEdit()
-tag_seacher.setPlaceholderText("Введіть тег:") #  пошук по тегу
-#--------------------
-# ставимо кнопки
-#--------------------
+text_searcher = QLineEdit()                                         # Пошук  по тексту
+text_searcher.setPlaceholderText('Введіть текст...')
+
+tag_searcher = QLineEdit()
+tag_searcher.setPlaceholderText('Введіть тег...')                   # Пошук  по тегу
+
+# ---------------------------
+#      Створення кнопок
+# ---------------------------
 make_note = QPushButton()
-make_note.setText("Створити замітку") # створюєм
+make_note.setStyleSheet('background-color: orange;')
+make_note.setText("Створити замітку")                   
 
-delet_note = QPushButton()
-delet_note.setText("Видалити замітку") # видаляєм
+delete_note = QPushButton()
+delete_note.setStyleSheet('background-color: orange;')
+delete_note.setText("Видалити замітку")                  
 
 save_note = QPushButton()
-save_note.setText("Зберигти замітку") # Зберігаємо
+save_note.setStyleSheet('background-color: orange;')
+save_note.setText("Зберегти замітку")                  
 
-seacher_for_text = QPushButton()
-seacher_for_text.setText("Шукати за текстом")
+search_for_text = QPushButton()
+search_for_text.setStyleSheet('background-color: orange;')
+search_for_text.setText("Шукати замітку за текстом")         
 
-seacher_for_tag = QPushButton()
-seacher_for_tag.setText("Шукати за тегом")
+search_for_tag = QPushButton()
+search_for_tag.setStyleSheet('background-color: orange;')
+search_for_tag.setText("Шукати замітку за тегом")           
 
 add_to_note = QPushButton()
-add_to_note.setText("Додати тег до замітки")
+add_to_note.setStyleSheet('background-color: rgb(152, 157, 249);')
+add_to_note.setText("Додати тег до замітки")                   
 
 unpin_to_note = QPushButton()
-unpin_to_note.setText("Відкрипити тег до замітки")
+unpin_to_note.setStyleSheet('background-color: rgb(152, 157, 249);')
+unpin_to_note.setText("Відкріпити тег від замітки")
+
 
 convert_to_txt = QPushButton()
-convert_to_txt.setText("Конвертувати до txt формату")
+convert_to_txt.setStyleSheet('background-color: rgb(152, 157, 249);')
+convert_to_txt.setText("Конвертувати до txt-формату")
 
 action_theme_btn = QPushButton()
-action_theme_btn.setText("Змінити тему")
+action_theme_btn.setStyleSheet('background-color: rgb(231, 188, 249);')
+action_theme_btn.setText("Змінити тему на чорну")
 
-#--------------------
-# макет
-#--------------------
-row1 = QHBoxLayout()
+# ---------------------------
+#   Розміщення на макет
+# ---------------------------
+
+row1 = QHBoxLayout()              # - гориз додати і видалити замітку
 row1.addWidget(make_note)
-row1.addWidget(delet_note)
+row1.addWidget(delete_note)
 
-row2 = QHBoxLayout()
+row2 = QHBoxLayout()              # - гориз дод. до замітки та відкріпити від замітки
 row2.addWidget(add_to_note)
 row2.addWidget(unpin_to_note)
 
-
-col1 = QVBoxLayout()
+col1 = QVBoxLayout()              # - вер. ввести текст
 col1.addWidget(text_editor)
 
 col2 = QVBoxLayout()
@@ -82,99 +104,195 @@ col2.addWidget(QLabel("Список заміток:"))
 col2.addWidget(list_widget_1)
 col2.addLayout(row1)
 col2.addWidget(save_note)
+   
 col2.addWidget(QLabel("Список тегів:"))
 col2.addWidget(list_widget_2)
-col2.addWidget(QLabel("Пошук даних:"))
+col2.addWidget(QLabel("Пошук данних:"))
+col2.addWidget(tag_searcher)
 col2.addWidget(text_searcher)
-col2.addWidget(tag_seacher)
 col2.addLayout(row2)
 
-col2.addWidget(seacher_for_tag)
-col2.addWidget(seacher_for_text)
+# Нижні 3 кнопки
+col2.addWidget(search_for_tag)
+col2.addWidget(search_for_text)
 col2.addWidget(convert_to_txt)
 col2.addWidget(action_theme_btn)
-#--------------------
+
 # Злиття
-#--------------------
-layout_note =  QHBoxLayout()
+layout_note = QHBoxLayout()
 layout_note.addLayout(col1)
 layout_note.addLayout(col2)
 
-window.setLayout(layout_note)
+# Макет на екран
+window.setLayout(layout_note) 
+
+# ---------------------------------------------------------[ Функціонал програми ]---------------------------------------------------------------------
+
+# ------------------- [ Замітки ] -------------------
 
 #  отримуємо текст із замітки з виділеною назвою та відображаємо його в полі редагування
 def show_notes():
-    global key                                                # назва замітки - ключ
-    key = list_widget_1.selectedItems()[0].text()     # дізнаємось на яку записку клікнули
+    global key                                        # зберігати назву замітки
+    key = list_widget_1.selectedItems()[0].text()     # дізнаємось на яку замітку клікнули
     list_widget_2.clear()                             # очищаємо поле із тегами
     text_editor.setText(notes[key]["text"])           # відобразили текст замітки
     list_widget_2.addItems(notes[key]["tag"])         # відобразили теги замітки
 
 
+#  додаємо нову замітку 
 def add_notes():
-    note_name,ok  = QInputDialog.getText(window,"Додати замітку","Назва замітки:")
+    note_name,ok = QInputDialog.getText(window,'Додати замітку',"Назва замітки")    # - Створити вікно QInputDialog з назвою note_name і зчитати текст
     if note_name and ok:
-        list_widget_1.addItems(note_name)
-        notes[note_name]= {"text":"","tag":""}
-    writeToFile()
+        list_widget_1.addItem(note_name)                                            # - Додає замітку до віджету (списку заміток)
+        notes[note_name] = {"text":"","tag":[]}                                     # - Додає до списку notes новий обєкт - але з  пустими поки полями
+    writeToFile()                                                                   # - Викликаємо фукнцію для запису в Json фалу нові замітки
 
+
+#  видалити вибрану замітку 
 def delete_notes():
-    if list_widget_1.currentItem():
-        if key in notes:
-            notes.pop(key)
-        
-        text_editor.clear()
-        list_widget_1.clear()
-        list_widget_2.clear()
-        
-        list_widget_1.addItems(notes)
-        writeToFile()
+    if list_widget_1.currentItem():          # - якщо вибрана замітка
+        if key in notes:                     # - і є така замітка в словнику notes (за ключем)
+            notes.pop(key)                   # - pop видаляє з словника
+            
+            text_editor.clear()              # - очищаємо віджети
+            list_widget_2.clear()
+            list_widget_1.clear()
+            list_widget_1.addItems(notes)    # - оновлюємо віджет списку заміток
+            writeToFile()                    # - перезаписуємо файл
 
+
+#  зберегти замітку 
 def save_notes():
-    if list_widget_1.currentItem():
-        key = list_widget_1.currentItem().text()
-        notes["text"] = text_editor.toPlainText()
-        writeToFile()
+    if list_widget_1.currentItem():                       # - якщо вибрана замітка
+        key = list_widget_1.currentItem().text()          # - отримати назву вибраної замітки
+        notes[key]['text'] = text_editor.toPlainText()   # - записати у словник notes з текстом з віджену
+        writeToFile() 
+
+
+# шукати по замітці
+def search_for_note():
+
+    text_to_search = text_searcher.text()
+    if search_for_text.text() == 'Шукати замітку за текстом':
+        filtered_notes = {}
+        for key, note_data in notes.items():
+            if text_to_search in note_data['text']:
+                filtered_notes[key] = note_data
+
+        search_for_text.setText('Скинути пошук')
+        list_widget_1.clear()
+        list_widget_1.addItems(filtered_notes.keys())
+        list_widget_2.clear()
+        text_editor.clear()
+
+    elif search_for_text.text() == 'Скинути пошук':
+        search_for_text.setText('Шукати замітку за текстом')
+        list_widget_1.clear()
+        list_widget_1.addItems(notes.keys())
+        list_widget_2.clear()
+        text_editor.clear()
+
+
+
+#Теги
+
+# Додати тег до замітки
+def add_tag():
+    key = list_widget_1.selectedItems()[0].text() # Якщо в списку заміток обрана замітка
+
+    if key in notes: #  чи замітк існує в списку
+        tag_name,ok = QInputDialog.getText(window,'Додати тег',"Назва тегу")
+        if tag_name and ok:
+            notes[key]["tag"].append(tag_name)
+            writeToFile() 
+        
+        tag_searcher.clear()
+        list_widget_2.addItems(notes[key]["tag"])
 
 
 
 
+def delete_tag():
+    if key in notes:
+        current_item = list_widget_2.currentItem()
+        if current_item:
+            tag_name = current_item.text()
+            notes[key]["tag"].remove(tag_name)
+            list_widget_2.takeItem(list_widget_2.row(current_item))
+            writeToFile()
+
+
+# пошук по тегу
+def search_note_for_tag():
+
+    tag = tag_searcher.text()                                               # Отримуємо текст, введений у поле пошуку тегів
+    if search_for_tag.text() == 'Шукати замітку за тегом':                  # Перевіряємо, чи кнопка пошуку по тегу має текст 'Шукати замітку за тегом'
+        filtered_notes = {}                                                     # Створюємо словник для збереження відфільтрованих заміток
+        for key in notes:                                                       # Перебираємо всі замітки в словнику notes
+            if tag in notes[key]['tag']:                                        # Якщо введений тег є в списку тегів замітки
+                filtered_notes[key] = notes[key]                                    # Додаємо замітку до відфільтрованих
+        search_for_tag.setText('Скинути пошук')                                 # Змінюємо текст кнопки на 'Скинути пошук'
+
+        list_widget_1.clear()
+        list_widget_1.addItems(filtered_notes)                              # Додаємо відфільтровані замітки у віджет
+        list_widget_2.clear()
+        text_editor.clear()
+
+    elif search_for_tag.text() == 'Скинути пошук':                          # Якщо кнопка має текст 'Скинути пошук'
+            search_for_tag.setText('Шукати замітку за тегом')               # Змінюємо текст кнопки на 'Шукати замітку за тегом'
+            list_widget_1.clear()
+            list_widget_1.addItems(notes.keys())                            # Додаємо всі замітки у відже
+            list_widget_2.clear()
+            text_editor.clear()
+
+def change_theme():
+    window.setStyleSheet(' background-color:blue;color:white;front-size:20px; border: 1px solid white;')
+    text_editor.setStyleSheet('background-color:blue;color:white')
+
+    list_widget_1.setStyleSheet('background-color:blue;color:white')
+    list_widget_2.setStyleSheet('background-color:blac;color:white')
+    text_editor.setStyleSheet('background-color:grey;color:white')
+
+    text_searcher.setStyleSheet('background-color:brown;color:white')
+    tag_searcher.setStyleSheet('background-color:black red;color:white')
+    
+    make_note.setStyleSheet('background-color:purple;color:white')
+    delete_note.setStyleSheet('background-color:black blue;color:white')
+    save_note.setStyleSheet('background-color:black yellow;color:white')
+
+    search_for_text.setStyleSheet('background-color:green;color:white')
+    search_for_tag.setStyleSheet('background-color:yellow;color:white')
+
+    add_to_note.setStyleSheet('background-color:red;color:white')
+    unpin_to_note.setStyleSheet('background-color:pink;color:white')
+    convert_to_txt.setStyleSheet('background-color:cyan;color:white')
+
+    action_theme_btn.setStyleSheet('background-color:orange;color:white')
 
 
 
+'''Запуск програми'''
+# підключення обробки подій
+list_widget_1.itemClicked.connect(show_notes)
 
+#  підключення обробки подій
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Підключаєм функцію віджету
-list_widget_1.clicked.connect(show_notes)
 make_note.clicked.connect(add_notes)
-delet_note.clicked.connect(delete_notes)
+delete_note.clicked.connect(delete_notes)
 save_note.clicked.connect(save_notes)
 
+add_to_note.clicked.connect(add_tag)
+unpin_to_note.clicked.connect(delete_tag)
+search_for_tag.clicked.connect(search_note_for_tag)
+search_for_text.clicked.connect(search_for_note)
+action_theme_btn.clicked.connect(change_theme)
 
 
-
-
-# Зчитуємо файл
-with open("notes.json","r") as file:
+# Зчитати файл - щоб з самого початку вже видно було всі замітки
+with open("notes.json", "r") as file:
     notes = json.load(file)
 list_widget_1.addItems(notes)
 
-
-
+# --------------------------- Закриття програми та показ
 window.show()
 app.exec_()
